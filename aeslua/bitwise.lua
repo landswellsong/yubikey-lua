15e3358553343aeccf33af8c0da80460c524190e
Oleksiy Protas <elfy.ua@gmail.com>
2016-08-02 01:01:16 +0300

    Anti-loop call measure.

--- aeslua/bitwise.lua
+++ aeslua/bitwise.lua
@@ -1,7 +1,11 @@
+local bitops = {}
+
 -- Lua 5.2 replaced bitlib with bit32 builtin
 if _VERSION:match("%d.(%d)") == "2" then
-    return bit32
+    bitops = bit32
+    return bitops
 -- Oh, it's Lua 5.1, let's require bitlib then
 else
-    return require("bit");
+    bitops = require("bit")
+    return bitops
 end
